a75c6d4d2d4353dd32a0fb585c1686896422b419
Daniel Dolan <danieldolan7@gmail.com>
2020-10-09 23:13:43 -0400

    Assignment 4 fixed delete

--- app/components/user-display.js
+++ app/components/user-display.js
@@ -3,7 +3,13 @@ import { tracked } from '@glimmer/tracking';
 
 export default class UserDisplayComponent extends Component {
     @tracked AllInfo = [];
+    constructor(){
+        super(...arguments)
+        this.AllInfo = this.args.allInfo
+        }
 
-
+        clearIndex(index){
+            this.AllInfo.removeAt(index);
+        }
 }
 
